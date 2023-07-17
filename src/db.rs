2502ad86bd1698b41b636cb0d8b5b88f98633ad8
Tim Abell <tim@timwise.co.uk>
2023-07-17 01:18:29 +0100

    Failed attempt to get GPT to remember invites
    Produced this which also doesn't compile.

--- src/db.rs
+++ src/db.rs
@@ -3,21 +3,21 @@ use diesel::PgConnection;
 use diesel::prelude::*;
 use diesel::result::Error;
 
-use super::models::User;
+use super::models::{User, Invite};
 
 type DBPool = Pool<ConnectionManager<PgConnection>>;
 
-pub struct UserRepository {
+pub struct Repository {
     pool: DBPool,
 }
 
-impl UserRepository {
+impl Repository {
     pub fn new(database_url: &str) -> Self {
         let manager = ConnectionManager::<PgConnection>::new(database_url);
         let pool = Pool::builder()
             .build(manager)
             .expect("Failed to create pool.");
-        UserRepository { pool }
+        Repository { pool }
     }
 
     pub fn create_user(&self, new_user: User) -> Result<User, Error> {
@@ -31,5 +31,16 @@ impl UserRepository {
         Ok(new_user)
     }
 
-    // Additional methods to fetch, update, delete users...
+    pub fn create_invite(&self, new_invite: Invite) -> Result<Invite, Error> {
+        use super::schema::invites::dsl::*;
+
+        let connection = self.pool.get().expect("Failed to fetch a connection.");
+        diesel::insert_into(invites)
+            .values(&new_invite)
+            .execute(&connection)?;
+
+        Ok(new_invite)
+    }
+
+    // Additional methods to fetch, update, delete users and invites...
 }
