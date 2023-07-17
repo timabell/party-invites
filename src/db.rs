186a0496240f42241ad89968759df9318fba55c1
Tim Abell <tim@timwise.co.uk>
2023-07-17 01:18:06 +0100

    Very strange fix to db.rs from GPT!
    at this point I think I've run out of gpt context because it's
    producing completely different things for db.rs now and seems to have
    forgotten abut invites entirely and is now playing with Repositories.
    
    Committing for historical interest, and because it's past bed time....

--- src/db.rs
+++ src/db.rs
@@ -1,36 +1,35 @@
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::PgConnection;
 use diesel::prelude::*;
-use crate::models::invite::{Invite, NewInvite};
-use crate::schema::invites::dsl::*;
+use diesel::result::Error;
 
-pub fn create_invite<'a>(conn: &PgConnection, name: &'a str, phone: &'a str) -> Invite {
-    let new_invite = NewInvite {
-        name,
-        phone,
-    };
+use super::models::User;
 
-    diesel::insert_into(invites)
-        .values(&new_invite)
-        .get_result(conn)
-        .expect("Error saving new invite")
+type DBPool = Pool<ConnectionManager<PgConnection>>;
+
+pub struct UserRepository {
+    pool: DBPool,
 }
 
-pub fn read_invites(conn: &PgConnection) -> Vec<Invite> {
-    invites.load::<Invite>(conn).expect("Error loading invites")
-}
+impl UserRepository {
+    pub fn new(database_url: &str) -> Self {
+        let manager = ConnectionManager::<PgConnection>::new(database_url);
+        let pool = Pool::builder()
+            .build(manager)
+            .expect("Failed to create pool.");
+        UserRepository { pool }
+    }
 
-pub fn update_invite(conn: &PgConnection, invite: &Invite) -> bool {
-    let rows = diesel::update(invites.filter(id.eq(&invite.id)))
-        .set(invite)
-        .execute(conn)
-        .expect("Error updating invite");
+    pub fn create_user(&self, new_user: User) -> Result<User, Error> {
+        use super::schema::users::dsl::*;
 
-    rows > 0
-}
+        let connection = self.pool.get().expect("Failed to fetch a connection.");
+        diesel::insert_into(users)
+            .values(&new_user)
+            .execute(&connection)?;
 
-pub fn delete_invite(conn: &PgConnection, invite_id: i32) -> bool {
-    let rows = diesel::delete(invites.filter(id.eq(invite_id)))
-        .execute(conn)
-        .expect("Error deleting invite");
+        Ok(new_user)
+    }
 
-    rows > 0
+    // Additional methods to fetch, update, delete users...
 }
